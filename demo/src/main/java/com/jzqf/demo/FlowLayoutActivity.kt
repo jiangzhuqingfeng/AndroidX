729e823f774a33c6cdad0df88746ba817f080985
jiangzhuqingfeng <872235631@qq.com>
2020-01-20 10:58:02 +0800

    测试tablayout

---
+++ demo/src/main/java/com/jzqf/demo/FlowLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.jzqf.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlowLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow_layout)
+    }
+}
